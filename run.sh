d70cee627e8ca17fd9cf89cea9e27c552982f3cf
Philippe Vaucher <philippe.vaucher@gmail.com>
2025-02-13 16:33:41 +0100

    Add ability to pass MP4FILE

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@
 set -e
 
 # Check if arguments are provided
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <IP_ADDRESS>"
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <IP> <MP4FILE>"
     exit 1
 fi
 
 IP="$1"
+MP4FILE="$2"
 INTERFACE='eth0' # inside docker it's always eth0
 
 # Run the Docker container with the provided arguments
@@ -18,5 +19,7 @@ docker run --rm \
     -p "$IP:554:554" \
     -p "$IP:1000:1000" \
     -p "$IP:8554:8554" \
+    -v "$MP4FILE:$MP4FILE" \
     -e INTERFACE="$INTERFACE" \
+    -e MP4FILE="$MP4FILE" \
     onvif-camera-mock-onvif-camera /onvif-camera-mock/main.py
